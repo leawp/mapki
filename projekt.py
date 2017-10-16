637f3b2364693180a4fe8aeab3a97ab45e6f6f5b
Pawel Handrysik <pawel_handrysik@bitbucket.org>
2017-10-16 13:25:59 +0200

    bruteforce recursion

--- projekt.py
+++ projekt.py
@@ -1,2 +1,56 @@
-print("hallo welt")
-print("yo")
+mapa = """10 20
+####################
+#      $           #
+#                  #
+#                  #
+#                  #
+#                  #
+#           @      #
+#                  #
+#                  #
+####################
+"""
+
+visited = []
+def findpath(mapa):
+    mapa = mapa.split("\n")
+    size = map(int, mapa[0].split(' '))
+    mapa = mapa[1:]
+    mapa = map(list, mapa)
+    start = None
+    for y in range(size[0]):
+        for x in range(size[1]):
+            if mapa[y][x] == '@':
+                start = x,y
+                break
+        if start != None:
+            break
+    _findpath(mapa, start[0], start[1])
+
+    visited.remove(start)
+    for pos in visited:
+        mapa[pos[1]][pos[0]] = '.'
+
+
+    mapa = map(''.join, mapa)
+    print('\n'.join(mapa))
+
+
+
+
+def _findpath(mapa, x,y):
+    if mapa[y][x] == '$':
+        return
+    visited.append((x,y))
+    if mapa[y][x+1] != '#' and (x+1, y) not in visited:
+        _findpath(mapa, x+1, y)
+    if mapa[y][x-1] != '#' and (x-1, y) not in visited:
+        _findpath(mapa, x-1, y)
+    if mapa[y+1][x] != '#' and (x, y+1) not in visited:
+        _findpath(mapa, x, y+1)
+    if mapa[y-1][x] != '#' and (x, y-1) not in visited:
+        _findpath(mapa, x, y-1)
+
+
+findpath(mapa)
+print mapa
